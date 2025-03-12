3be0f3a272b456bcbabbf7b558b14399171a05f1
William Joel Chávez López <williamjoel1909@gmail.com>
2025-03-12 09:03:29 -0500

    sidebar v1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
+import SDbar from './components/SDbar.vue';
 </script>
 
 <template>
   <RouterView />
+  <SDbar />
 </template>
